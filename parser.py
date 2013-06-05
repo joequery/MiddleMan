8da8d73fff3fa04274f765c5dcd30b8e1ede7028
Joseph McCullough <joseph@vertstudios.com>
2013-06-04 23:36:06 -0500

    More testing infrastructure

--- parser.py
+++ parser.py
@@ -1,5 +1,38 @@
-import json
+# Functions for parsing and condensing JSON based upon user preferences.
 
-def trim_json_whitespace(rawJSON):
-    j = json.loads(rawJSON)
-    return json.dumps(j)
+import json
+import re
+
+def extract_references(scheme):
+    """
+    Extract all references, which are of the form ${{mykey}}, from the scheme.
+    A scheme may look like
+
+        {
+            "time": ${{timestamp}},
+            "id": ${{user_id}}
+        }
+
+    Where timestamp and user_id are keys in a JSON result.
+    """
+    pattern = "\${{[^}]+}}"
+    results = re.findall(pattern, scheme)
+    return results
+
+def apply_scheme_to_json(scheme, rawJSON):
+    """
+    Apply a user specified scheme to JSON. The references in the scheme will
+    have their corresponding values replaced. Returns the dict representing the
+    json.
+    """
+    return {"key1":"myvalue1"}
+
+def extract_reference_value_from_json(reference, rawJSON):
+    """
+    Extract the value of the key represented by reference from the JSON.
+    For example, 
+    >>> v = extract_reference_value_from_json("${{mykey}}", '{"mykey": "1"}')
+    >>> v
+    "1"
+    """
+
--- tests/parser_test.py
+++ tests/parser_test.py
@@ -3,9 +3,28 @@ from tests.testhelpers import testfile
 
 import parser
 
+class ReferenceValueExtraction(unittest.TestCase):
+    def test_extract_simple_key_value_pair(self):
+        self.assertTrue(True)
+
 class JSONParserTest(unittest.TestCase):
-    def test_trim_json_whitespace(self):
-        ipJSON = testfile("httpbinIP.txt")
-        expected = '{"origin": "74.192.112.168"}'
-        trimmedJSON = parser.trim_json_whitespace(ipJSON)
-        self.assertEqual(expected, trimmedJSON)
+    def test_extract_references_simple(self):
+        scheme = testfile("simple_scheme_1.txt")
+        references = parser.extract_references(scheme)
+        expected = ['${{mykey1}}', '${{timezone}}']
+        self.assertListEqual(expected, references)
+
+    def test_apply_scheme_to_json_simple(self):
+        scheme = testfile("simple_scheme_1.txt")
+        rawJSON = testfile("simple_json_1.txt")
+
+        jsonAfterApplication = parser.apply_scheme_to_json(scheme, rawJSON)
+        expected = {
+            "key1": "myvalue1", 
+            "key2": "some hardcoded value", 
+            "time": "US/Central"
+        }
+
+        self.assertDictEqual(expected, jsonAfterApplication)
+
+

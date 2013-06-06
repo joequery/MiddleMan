61c2c44df8bd715db90f07446ba285e503ab8145
Joseph McCullough <joseph@vertstudios.com>
2013-06-06 04:31:26 -0500

    Improve organization of parser usage tests

--- tests/parser/usage.py
+++ tests/parser/usage.py
@@ -5,7 +5,7 @@ from tests.testhelpers import testfile
 import parser
 import json
 
-class ReferenceUsage(unittest.TestCase):
+class ReferenceHelpers(unittest.TestCase):
     def test_extract_references_simple(self):
         scheme = """
         {
@@ -18,6 +18,20 @@ class ReferenceUsage(unittest.TestCase):
         expected = ["${['mykey1']}$", "${['timezone']}$"]
         self.assertListEqual(expected, references)
 
+    def test_extract_simple_key_value_pair(self):
+        rawJSON = """
+        {
+            "mykey1": "myvalue1",
+            "timezone": "US/Central"
+        }
+        """
+        reference = '${["mykey1"]}$'
+        expected = "myvalue1"
+        extracted = parser.extract_reference_value_from_json(reference, rawJSON)
+        self.assertEqual(expected, extracted)
+
+
+class ReferenceUseCases(unittest.TestCase):
     def test_apply_scheme_to_json_simple(self):
         scheme = """
         {
@@ -61,16 +75,3 @@ class ReferenceUsage(unittest.TestCase):
         result = parser.apply_scheme_to_json(scheme, rawJSON).strip()
         expected = "httpbin.org, 74.192.112.168"
         self.assertEqual(expected, result)
-
-    def test_extract_simple_key_value_pair(self):
-        rawJSON = """
-        {
-            "mykey1": "myvalue1",
-            "timezone": "US/Central"
-        }
-        """
-        reference = '${["mykey1"]}$'
-        expected = "myvalue1"
-        extracted = parser.extract_reference_value_from_json(reference, rawJSON)
-        self.assertEqual(expected, extracted)
-

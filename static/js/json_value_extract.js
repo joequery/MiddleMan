2d881d657d56a7db74d156f769e366d837adb24d
Joseph McCullough <joseph@vertstudios.com>
2014-05-20 11:04:57 +0000

    JSON traversal didn't work out. Try again later

--- static/js/json_value_extract.js
+++ static/js/json_value_extract.js
@@ -1,25 +1,19 @@
 $(function(){
     var DomOutlineHandlers = {
         'click': function(e){
-            console.log('Clicked element:', e);
+            console.log("You clicked: ", e);
         },
         'mouseover': function(e){
             $(".DomOutline").show();
-            console.log('Hovered over element:', e);
         },
         'mouseout': function(e){
             $(".DomOutline").hide();
-            console.log('Leaving element:', e);
         },
     }
-    var DOutline = DomOutline({ handlers: DomOutlineHandlers, filter: 'code span' })
-    /*
     var DOutline = DomOutline({
-        filter: 'code span',
-        onClick: function(e){
-            console.log('Clicked element:', e);
-        }
+        handlers: DomOutlineHandlers,
+        filter: 'code span:not(.hljs-attribute)'
     })
-    */
     DOutline.start()
+
 });

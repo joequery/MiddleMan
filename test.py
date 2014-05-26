fc92ab8de60f0c7f88596add72433faa6a619db1
Joseph McCullough <joseph@vertstudios.com>
2014-05-26 07:57:23 +0000

    Remove parserform webapp test

--- test.py
+++ test.py
@@ -5,5 +5,4 @@ if __name__ == '__main__':
     from tests.parser.errors import *
     from tests.parser.grammar import *
     from tests.parser.usage import *
-    from tests.webapp.parserform import *
     unittest.main(verbosity=2)
